bd135865b35233270f254193ebddaeac5a702c65
mdrihan34 <mdrihan0019@gmail.com>
2025-04-03 22:21:26 +0600

    viwe project fetch

--- client-side-portfolio/src/dashboard/ViweProject.jsx
+++ client-side-portfolio/src/dashboard/ViweProject.jsx
@@ -4,7 +4,7 @@ import ProjectCards from './ProjectCards';
 
 const ViweProject = () => {
   const [projects, setProjects] = useState([]);
-  fetch("https://server-site-portfolio-czyvsxb2r-rihan2045s-projects-8a229069.vercel.app/project", {
+  fetch("https://portfolio-w7p2.vercel.app/project", {
     method: "GET",
     headers: {
       "Content-Type": "application/json"
@@ -12,17 +12,20 @@ const ViweProject = () => {
     mode: "cors"
   })
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) =>
+     
+     setProjects(data))
+  
     .catch((error) => console.error("Error:", error));
   
   // console.log(projects)
   return (
     <div className="container gap-5 grid grid-cols-3 mx-auto p-4">
- {/* {
+ {
   projects.map((project) => (
    <ProjectCards project={project} key={project._id}></ProjectCards>
   ))
-} */}
+}
     </div>
   );
 };
